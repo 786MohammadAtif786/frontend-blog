d328314a1d0f9e9b72b45bb3eccd4b67e9b963cf
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-23 23:50:31 +0530

    fix: admin ui1

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 // export default function AdminDashboard() {
 //   const [blogs, setBlogs] = useState([]);
